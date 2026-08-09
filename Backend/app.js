657d66e456a2a6b27fb339a6660c78bc033166f2
Ayush Sinha <as.ayushh1@gmail.com>
2026-08-09 13:39:37 +0530

    db server setup

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,6 @@ import express from "express";
 import cors from "cors"
 
 const app = express()
-
 app.use(cors())
 
 app.get('/', 
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,15 @@
 import http from 'http'
 import app from './app.js'
 
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+import connectDB from "./src/db/db.js";
+
+
+connectDB()
+
+
 const port = process.env.PORT  || 3000
 
 const server =http.createServer(app)
