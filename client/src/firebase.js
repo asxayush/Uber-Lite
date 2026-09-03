4d81960ad79cea7cd3fc392cf40fd10195112adc
Ayush Sinha <as.ayushh1@gmail.com>
2026-09-03 17:54:52 +0530

    feat: update Firebase configuration to use environment variables and add .env.example file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,14 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyAIj19BZ1zERZvDNbJX9rkXqWmaxBsGPAQ",
-  authDomain: "uber-lite-87d7e.firebaseapp.com",
-  projectId: "uber-lite-87d7e",
-  
-  
-  
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 // Initialize Firebase
@@ -19,20 +15,20 @@ const provider = new GoogleAuthProvider();
 
 // Call this function when the user clicks your "Sign in with Google" button
 export const handleGoogleSignIn = async () => {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    
-    // This gives you a Google Access Token to access Google APIs if needed
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+    try {
+        const result = await signInWithPopup(auth, provider);
 
-    // The signed-in user info
-    const user = result.user;
-    console.log("Success! Signed in as:", user.displayName);
-    return user;
-  } catch (error) {
-    console.error("Error during sign-in:", error.message);
-  }
+        // This gives you a Google Access Token to access Google APIs if needed
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info
+        const user = result.user;
+        console.log("Success! Signed in as:", user.displayName);
+        return user;
+    } catch (error) {
+        console.error("Error during sign-in:", error.message);
+    }
 };
 
-export { auth, provider,  };
+export { auth, provider, };
